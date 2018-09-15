74168b112d4c56d8783a52c69db984266abd8f1f
Antonius Yonathan <atomicbomber.email@gmail.com>
2018-09-15 07:13:44 +0700

    Created a shared layout and navbar. Old views now use them.

--- resources/views/process/control_panel.blade.php
+++ resources/views/process/control_panel.blade.php
@@ -1,27 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title> Document Similarity | Control Panel </title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-</head>
+@extends('shared.layout')
 
-<body>
-    <div class="container mt-5">
-        <h1>
-            Panel Kendali Utama
-        </h1>
-        <p class="lead"> Penghitung Similaritas Dokumen dengan Teknik <em> Cosine Similarity </em> </p>
+@section('title', 'Document Similarity | Control Panel')
 
-        <div id="root-process-text">
-            <ProcessText/>
-        </div>
+@section('content')
+<div class="container mt-5">
+    <h1>
+        Panel Kendali Utama
+    </h1>
+    <p class="lead"> Penghitung Similaritas Dokumen dengan Teknik <em> Cosine Similarity </em> </p>
 
+    <div id="root-process-text">
+        <ProcessText/>
     </div>
 
-    <script src="{{ asset('js/app.js') }}"></script>
-</body>
-</html>
+</div>
+@endsection
