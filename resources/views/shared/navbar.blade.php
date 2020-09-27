47e4d1e9cdd704fe9e32e84e68c4e4404ce4aa77
Antonius Yonathan <james.patrick.keegan.business@gmail.com>
2020-09-27 23:20:41 +0700

    Updates.

--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -9,6 +9,18 @@
             <a class="nav-item nav-link {{ Route::is('process.*') ? 'active' : '' }}" href="{{ route('process.control_panel') }}"> Pasangan Dokumen </a>
             <a class="nav-item nav-link {{ Route::is('thesis.*') ? 'active' : '' }}" href="{{ route('thesis.index') }}"> Skripsi </a>
             </div>
+
+            <ul class="navbar-nav ml-auto">
+                <form action="{{ route("logout") }}"
+                      method="POST"
+                >
+                    @csrf
+                    @method("POST")
+                    <button class="btn btn-danger btn-sm">
+                        Logout <i class="fa fa-sign-out"></i>
+                    </button>
+                </form>
+            </ul>
         </div>
     </div>
 </nav>
